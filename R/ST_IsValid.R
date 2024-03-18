e382df3ee5c973f1b6067a8dddc223ad99a6a511
Ed Ruiz <ecr7407@gmail.com>
2024-03-18 16:00:52 -0400

    feat: add ST_IsValid()

---
+++ R/ST_IsValid.R
@@ -0,0 +1,46 @@
+#' Determine if geometry is valid
+#'
+#' @param tbl name of a table in a duckdb database
+#'
+#' @return boolean vector of whether each geometry in tbl is valid
+#' @export
+#' @keywords spatial_prop
+#' @examples
+#' # Create a data.frame with x and y coordinates and attributes
+#' coordinates <- data.frame(x = c(100, 200, 300), y = c(500, 600, 700))
+#' attributes <- data.frame(id = 1:3, name = c("A", "B", "C"))
+#'
+#' # Combine the coordinates and attributes
+#' dummy_data <- cbind(coordinates, attributes)
+#'
+#' # Create a SpatVector from the data.frame
+#' dummy_spatvector <- terra::vect(dummy_data, geom = c("x", "y"))
+#'
+#' # Create a duckdb connection
+#' duckdb_conn = DBI::dbConnect(duckdb::duckdb(), ":memory:")
+#'
+#' # Create a duckdb table with spatial points
+#' db_points = dbSpatial(conn = duckdb_conn,
+#'                       name = "spatVector_proxy",
+#'                       value = dummy_spatvector,
+#'                       overwrite = TRUE)
+#'                       
+#' # Get extent of the table
+#' ST_IsValid(db_points)
+ST_IsValid <- function(tbl){
+  # check inputs
+  if (missing(tbl)) {
+    stop("Please provide a table name")
+  }
+  
+  # check that tbl is from a duckdb connection
+  if (!inherits(tbl, "tbl_duckdb_connection")) {
+    stop("Please provide a duckdb table")
+  }
+  
+  res <- tbl |>
+    dplyr::mutate(is_valid = ST_IsValid(geom)) |>
+    dplyr::pull(is_valid)
+  
+  res
+}
