aa51db746452146814ac61f5f654f9ca428526f2
Ed Ruiz <ecr7407@gmail.com>
2024-03-18 16:01:02 -0400

    feat: add ST_Point()

---
+++ R/ST_Point.R
@@ -0,0 +1,14 @@
+ST_Point <- function(conn, name, x_colName, y_colName){
+  # input validation
+  .check_con(conn)
+  .check_name(name)
+  
+  # create point geometry
+  point_query = paste0("CREATE OR REPLACE TABLE ", name, " AS ",
+                       " SELECT *, ST_Point(", x_colName, ", ", y_colName, ") AS geom FROM ", name)
+  invisible(DBI::dbExecute(conn, point_query))
+  
+  # Show preview
+  dplyr::name(conn, name)
+  
+}
