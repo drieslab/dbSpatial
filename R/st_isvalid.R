419ee6bacb2840535a852c2e64f667452623bc88
Eddie Ruiz <32622519+Ed2uiz@users.noreply.github.com>
2024-03-27 16:06:59 -0700

    fix: dplyr !! rlang::sym mismatch

--- R/st_isvalid.R
+++ R/st_isvalid.R
@@ -13,26 +13,26 @@
 #' @family geom_solo
 #' @examples
 #' # Create a data.frame with x and y coordinates and attributes
-coordinates <- data.frame(x = c(100, 200, 300), y = c(500, 600, 700))
-attributes <- data.frame(id = 1:3, name = c("A", "B", "C"))
-
-# Combine the coordinates and attributes
-dummy_data <- cbind(coordinates, attributes)
-
-# Create a SpatVector from the data.frame
-dummy_spatvector <- terra::vect(dummy_data, geom = c("x", "y"))
-
-# Create a duckdb connection
-duckdb_conn = DBI::dbConnect(duckdb::duckdb(), ":memory:")
-
-# Create a duckdb table with spatial points
-db_points = dbSpatial(conn = duckdb_conn,
-                      name = "spatVector_proxy",
-                      value = dummy_spatvector,
-                      overwrite = TRUE)
-
-# Get extent of the table
-st_isvalid(db_points)
+#' coordinates <- data.frame(x = c(100, 200, 300), y = c(500, 600, 700))
+#' attributes <- data.frame(id = 1:3, name = c("A", "B", "C"))
+#'
+#' # Combine the coordinates and attributes
+#' dummy_data <- cbind(coordinates, attributes)
+#'
+#' # Create a SpatVector from the data.frame
+#' dummy_spatvector <- terra::vect(dummy_data, geom = c("x", "y"))
+#'
+#' # Create a duckdb connection
+#' duckdb_conn = DBI::dbConnect(duckdb::duckdb(), ":memory:")
+#'
+#' # Create a duckdb table with spatial points
+#' db_points = dbSpatial(conn = duckdb_conn,
+#'                       name = "spatVector_proxy",
+#'                       value = dummy_spatvector,
+#'                       overwrite = TRUE)
+#'
+#' # Get extent of the table
+#' st_isvalid(db_points)
 st_isvalid <- function(tbl, geomName = "geom"){
   # check inputs
   .check_tbl(tbl = tbl)
