13eda4a4848f3a03fe038b907d0a67ab8f344d44
Eddie Ruiz <32622519+Ed2uiz@users.noreply.github.com>
2024-03-27 14:03:17 -0700

    feat: add new handlers for spatial_relationship funcs

--- R/input_validation.R
+++ R/input_validation.R
@@ -3,7 +3,7 @@
 #' @noRd
 .check_con <- function(conn){
   if(missing(conn)) {
-    stop("Please provide a connection")
+    stop("Please provide a db connection.")
   }
   
   if(!DBI::dbIsValid(conn)) {
@@ -11,7 +11,7 @@
   }
   
   if(!inherits(conn, "duckdb_connection")) {
-    stop("conn must be a duckdb connection. Use duckdb drv in DBI::dbConnect()")
+    stop("conn must be a duckdb connection. Use duckdb drv in DBI::dbConnect().")
   }
 }
 
@@ -41,12 +41,12 @@
 #' @noRd
 .check_tbl <- function(tbl){
   if (missing(tbl)) {
-    stop("Please provide a table name")
+    stop("Please provide a table name.")
   }
   
   # check that tbl is from a duckdb connection
   if (!inherits(tbl, "tbl_duckdb_connection")) {
-    stop("Please provide a duckdb table")
+    stop("Please provide a duckdb table.")
   }
 }
 
@@ -55,7 +55,7 @@
 #' @noRd
 .check_name <- function(name){
   if(missing(name)) {
-    stop("Please provide a table name")
+    stop("Please provide a table name.")
   }
   
   if(is.null(name)) {
@@ -63,12 +63,12 @@
   }
   
   if(!is.character(name)) {
-    stop("name must be a character string")
+    stop("name must be a character string.")
   }
   
   # if name starts with a number, add warning
   if(grepl("^[0-9]", name)) {
-    stop("Table names should not start with a number")
+    stop("Table names should not start with a number.")
   }
   
   # reserved name check
@@ -86,12 +86,12 @@
 .check_geomName <- function(tbl, geomName){
   
   if(!(geomName %in% colnames(tbl))) {
-    stop("geomName not found in tbl")
+    stop("geomName not found in tbl.")
   }
   
   # if name starts with a number, add warning
   if(grepl("^[0-9]", geomName)) {
-    stop("Table names should not start with a number")
+    stop("Table names should not start with a number.")
   }
   
   # reserved name check
@@ -99,12 +99,16 @@
                      "select", "from", "where", "group", "by", "limit",
                      "create", "table", "insert")
   if(geomName %in% reserved_names){
-    stop("geomName cannot be a RESERVED word. Try another geomName")
+    stop("geomName cannot be a RESERVED word. Try another geomName.")
   }
 }
 
 #' Input validation for overwrite arg
 #' @keywords internal
+#' @details
+#' set overwrite = "PASS" token for handling lazy tables in database
+#' that are not computed and have no name
+#' 
 #' @noRd
 .check_overwrite <- function(conn, overwrite, name){
   if(overwrite == 'PASS') {
@@ -117,5 +121,38 @@
   
   if(!overwrite & DBI::dbExistsTable(conn, name)){
     stop("Table already exists. Set overwrite = TRUE to overwrite the table.")
+  }
+}
+
+#' Input validation for spatial_relationships functions
+#' @keywords internal
+#' @details
+#' ensure two dbSpatial objects are in the same connection
+#' 
+#' @noRd
+.check_con_shared <- function(conn1, conn2){
+  # TODO: check if g1 and g2 share the same connection
+  # How to extract absolute path of the con object from g1, g2?
+  if(!all.equal(conn1, conn2)){
+    stop("conn1 and conn2 must be in the same database connection.")
+  }
+}
+
+#' Input validation for spatial_relationships functions
+#' @keywords internal
+#' @details
+#' ensure two dbSpatial objects are in the same connection
+#' 
+#' @noRd
+.check_cols_keep <- function(tbl, cols_keep){
+  # check that cols_keep is a valid character vector
+  if(cols_keep != "all"){
+    if(!is.character(cols_keep)){
+      stop("cols_keep must be a character vector.")
+    }
+    
+    if(!all(cols_keep %in% colnames(tbl))){
+      stop("cols_keep must be a subset of column names in tbl.")
+    }
   }
 }
