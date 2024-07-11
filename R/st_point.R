3c64868c8b3f94ed2d431897d7720c82a1914eba
Eddie Ruiz <32622519+Ed2uiz@users.noreply.github.com>
2024-07-11 15:33:46 -0400

    feat: change st_point to construct VIEW instead of TABLE --- TODO: refactor all data ingestion to use VIEW construction instead of TABLE

--- R/st_point.R
+++ R/st_point.R
@@ -4,8 +4,9 @@ ST_Point <- function(conn, name, x_colName, y_colName){
   .check_name(name)
   
   # create point geometry
-  point_query = paste0("CREATE OR REPLACE TABLE ", name, " AS ",
-                       " SELECT *, ST_Point(", x_colName, ", ", y_colName, ") AS geom FROM ", name)
+  point_query = paste0("CREATE OR REPLACE VIEW ", name, " AS ",
+                       " SELECT *, ST_Point(", x_colName, ", ", y_colName, 
+                       ") AS geom FROM ", name)
   invisible(DBI::dbExecute(conn, point_query))
   
   # Show preview
